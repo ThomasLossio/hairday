68a5909c84fce46451f751edafebc8865b0c97b2
Thomas Lossio <thomaslossio@hotmail.com>
2024-07-13 12:34:57 -0300

    Add schedule new service

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs"
+import { scheduleNew } from "../../services/schedule-new"
 
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
@@ -9,7 +10,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
   event.preventDefault()
 
   try {
@@ -28,7 +29,7 @@ form.onsubmit = (event) => {
     const when = dayjs(selectedDate.value).add(hour, "hour")
 
     const id = new Date().getTime()
-    console.log({
+    await scheduleNew({
       id, name, when
     })
   } catch (error) {
